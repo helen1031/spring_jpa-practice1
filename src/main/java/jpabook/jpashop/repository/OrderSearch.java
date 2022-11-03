e4cdca5b6c6a88be5e11ed219a8ddf569695746b
helen1031 <32950301+helen1031@users.noreply.github.com>
2022-11-04 06:26:19 +0900

    Order Web

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter @Setter
 public class OrderSearch {
 
     private String memberName;
     private OrderStatus orderStatus;
+
+
 }
